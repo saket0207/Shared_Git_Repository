db05cd37d78f1c87bef8920990342138128eb5e0
saket <mac2me.saket@gmail.com>
2016-03-15 00:59:53 +0530

    SpringDemo01-1st basic spring example

---
+++ src/main/src/org/kumar/planets/beans/Person.java
@@ -0,0 +1,17 @@
+package org.kumar.planets.beans;
+
+/**
+ * Created by saket on 3/15/2016.
+ */
+public class Person {
+    private int id;
+    private String name;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+}
